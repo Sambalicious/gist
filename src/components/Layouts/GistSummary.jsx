85d990511fdcdf0ae69a30143094a889a0431e34
Sambalicious <stemitope370@gmail.com>
2020-08-18 18:51:48 +0100

    code clean-up

--- src/components/Layouts/GistSummary.jsx
+++ src/components/Layouts/GistSummary.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import moment from 'moment';
-import { Box, Heading, Stack, Text, Avatar } from '@chakra-ui/core'
+import React from "react";
+import moment from "moment";
+import { Box, Heading, Stack, Text, Avatar } from "@chakra-ui/core";
 
-const GistSummary = ({gist}) => {
-    return (
-        <Box padding="2rem"  my={3} mx={5} boxShadow="lg" justifyContent="center" align="center" >
-            <Stack align="center" isInline>
-                <Avatar  name={`${gist.authorFirstname} ${gist.authorLastname}`}/>
-                <Stack ml={4}>
-                <Text autoCapitalize="true" >{gist.authorFirstname} {gist.authorLastname}</Text>
-                <Text fontSize="xs" color="gray.500">{moment(gist.createdAt.toDate()).calendar()} </Text>
-                </Stack>
-                
-            </Stack>
-            <Heading textAlign="center" as="h6" size="md" my={4} mx={"auto"}>{gist.title} </Heading>
-        </Box>
-    )
-}
+const GistSummary = ({ gist }) => {
+  return (
+    <Box
+      padding="2rem"
+      my={3}
+      mx={5}
+      boxShadow="lg"
+      justifyContent="center"
+      align="center"
+    >
+      <Stack align="center" isInline>
+        <Avatar name={`${gist.authorFirstname} ${gist.authorLastname}`} />
+        <Stack ml={4}>
+          <Text autoCapitalize="true">
+            {gist.authorFirstname} {gist.authorLastname}
+          </Text>
+          <Text fontSize="xs" color="gray.500">
+            {moment(gist.createdAt.toDate()).calendar()}
+          </Text>
+        </Stack>
+      </Stack>
+      <Heading textAlign="center" as="h6" size="md" my={4} mx={"auto"}>
+        {gist.title}
+      </Heading>
+    </Box>
+  );
+};
 
-export default GistSummary
+export default GistSummary;
